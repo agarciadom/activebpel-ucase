cc4d915bffad709d7fc07e4f590de2e2037b72c3
Antonio García Domínguez <antonio.garciadominguez@uca.es>
2010-09-03 20:11:21 +0200

    o.a.rt.jetty: added placeholder for main class

---
+++ org.activebpel.rt.jetty/src/main/java/org/activebpel/rt/jetty/JettyRunner.java
@@ -0,0 +1,13 @@
+package org.activebpel.rt.jetty;
+
+public class JettyRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
