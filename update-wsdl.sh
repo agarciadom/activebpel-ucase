f1cd1d717f27563b4adb0abb1e43fe2cb21f4be4
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-05-24 16:04:22 -0400

    Split off WSCoL into a separate module

--- update-wsdl.sh
+++ update-wsdl.sh
@@ -7,14 +7,14 @@
 # Licensed under the GPLv2
 
 WSDL_CM_URL="http://localhost:8080/ConfigurationManagerBeanService/ConfigurationManagerBean?wsdl"
-WSDL_CM_RT_BPEL=org.activebpel.rt.bpel/src/main/wsdl/configurationmanager.wsdl
+WSDL_CM_WSCOL=it.polimi.wscol/src/main/wsdl/configurationmanager.wsdl
 WSDL_CM_DYN_WAR=it.polimi.dynamo.war/src/main/wsdl/ConfigurationManagerBean.wsdl
 
 WSDL_ML_URL="http://localhost:8080/MonitorLoggerBeanService/MonitorLoggerBean?wsdl"
-WSDL_ML_RT_BPEL=org.activebpel.rt.bpel/src/main/wsdl/monitorlogger.wsdl
+WSDL_ML_WSCOL=it.polimi.wscol/src/main/wsdl/monitorlogger.wsdl
 WSDL_ML_DYN_WAR=it.polimi.dynamo.war/src/main/wsdl/MonitorLoggerBean.wsdl
 
-wget -O- "$WSDL_CM_URL" > "$WSDL_CM_RT_BPEL"
+wget -O- "$WSDL_CM_URL" > "$WSDL_CM_WSCOL"
 wget -O- "$WSDL_CM_URL" > "$WSDL_CM_DYN_WAR"
-wget -O- "$WSDL_ML_URL" > "$WSDL_ML_RT_BPEL"
+wget -O- "$WSDL_ML_URL" > "$WSDL_ML_WSCOL"
 wget -O- "$WSDL_ML_URL" > "$WSDL_ML_DYN_WAR"
