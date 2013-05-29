68310d2ed8e9b1292b9ee7118abf0d3ea41d8b22
Antonio Garcia-Dominguez <antonio.garciadominguez@uca.es>
2013-05-29 15:03:47 -0400

    wscol: reformat NodeWSCoL a bit while reading its code

--- it.polimi.wscol/src/main/java/it/polimi/monitor/nodes/NodeWSCoL.java
+++ it.polimi.wscol/src/main/java/it/polimi/monitor/nodes/NodeWSCoL.java
@@ -14,7 +14,7 @@
 
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 package it.polimi.monitor.nodes;
 
@@ -29,83 +29,99 @@ import antlr.Token;
 import antlr.collections.AST;
 
 /**
- * This abstract class represents a NodeWSCoL that is use for analyze a rule WSCoL define 
- * with WSCoL grammar. 
- * This node has two abstract methods that will be use for evaluate and have the monitor 
- * value of a single node. 
+ * This abstract class represents a NodeWSCoL that is use for analyze a rule
+ * WSCoL define with WSCoL grammar. This node has two abstract methods that will
+ * be use for evaluate and have the monitor value of a single node.
  * 
  * 
  * @author Luca Galluppi
- *
  */
 public abstract class NodeWSCoL extends BaseAST {
-	protected InputMonitor inputMonitor=null;
-	protected Logger logger=null;
-	protected ResultMonitor resultMonitor=null;
+	private static final long serialVersionUID = -7610762948410522700L;
+
+	protected InputMonitor inputMonitor = null;
+	protected Logger logger = null;
+	protected ResultMonitor resultMonitor = null;
 	protected String serializeTag;
-	//	strutture per memoria temporanea
+
 	/**
 	 * Create a new NodeWSCoL and set the logger for have more information.
 	 * 
 	 */
 	public NodeWSCoL() {
-		logger=Logger.getLogger("Monitor WSCoL");
+		logger = Logger.getLogger("Monitor WSCoL");
 	}
+
 	/**
-	 * This method is use for costruct a NodeWSCoL with his child derived from the WSCoL grammar.
+	 * This method is use for costruct a NodeWSCoL with his child derived from
+	 * the WSCoL grammar.
 	 * 
 	 * If some errors occur during the evaluation a WSCoLException is throw.
 	 * 
-	 * @param inputMonitor The inputMonitor information for evaluate the Node
-	 * @param aliases The aliases known by the node
-	 * @param tempAliases The aliasnodes known by the node
-	 * @throws WSCoLException If an error occur a WSCoLException is throw.
+	 * @param inputMonitor
+	 *            The inputMonitor information for evaluate the Node
+	 * @param aliases
+	 *            The aliases known by the node
+	 * @param tempAliases
+	 *            The aliasnodes known by the node
+	 * @throws WSCoLException
+	 *             If an error occur a WSCoLException is throw.
 	 */
-	public abstract void evaluate(InputMonitor inputMonitor, Aliases aliases , AliasNodes aliasNodes) throws WSCoLException;
+	public abstract void evaluate(InputMonitor inputMonitor, Aliases aliases,
+			AliasNodes aliasNodes) throws WSCoLException;
+
 	/**
-	 * This method is use for calculate the monitoring value of the node.
-	 * If some errors occur during the calculation a WSCoLException is throw.
+	 * This method is use for calculate the monitoring value of the node. If
+	 * some errors occur during the calculation a WSCoLException is throw.
+	 * 
 	 * @return The value of monitoring of the node.
-	 * @throws WSCoLException If an error occur a WSCoLException is throw.
+	 * @throws WSCoLException
+	 *             If an error occur a WSCoLException is throw.
 	 */
 	public abstract Object getMonitoringValue() throws WSCoLException;
-	
-	//richiesto da baseast
+
+	// richiesto da baseast
 	public void initialize(int t, String txt) {
 	}
-	//richiesto da baseast
+
+	// richiesto da baseast
 	public void initialize(AST t) {
 	}
-	//richiesto da baseast
+
+	// richiesto da baseast
 	public void initialize(Token tok) {
 	}
+
 	/**
-	 * This method is use for add to the ResultMonitor the value and informations of monitoring the node. 
-	 *
+	 * This method is use for add to the ResultMonitor the value and
+	 * informations of monitoring the node.
 	 */
 	public void calculateResultMonitor() {
 		try {
 			Object res = getMonitoringValue();
 			if (res instanceof Boolean) {
-				resultMonitor.addValue((Boolean)res);
-				resultMonitor.addMessage("<"+serializeTag+">"+res+"</"+serializeTag+">");
-			} else if (res instanceof String )
-				resultMonitor.addMessage((String)res);
-			else if ( res instanceof Number)
-				resultMonitor.addMessage(((Number)res).toString());
+				resultMonitor.addValue((Boolean) res);
+				resultMonitor.addMessage("<" + serializeTag + ">" + res + "</"
+						+ serializeTag + ">");
+			} else if (res instanceof String)
+				resultMonitor.addMessage((String) res);
+			else if (res instanceof Number)
+				resultMonitor.addMessage(((Number) res).toString());
 		} catch (WSCoLException e) {
 			resultMonitor.addMessage(e.getMessage());
 		}
-		
+
 	}
+
 	/**
-	 * This method is use for set in the ResultMonitor so the node can save information about
-	 * the monitoring.
+	 * This method is use for set in the ResultMonitor so the node can save
+	 * information about the monitoring.
 	 * 
-	 * @param resultMonitor The resultMonitor to set.
+	 * @param resultMonitor
+	 *            The resultMonitor to set.
 	 */
 	public void setResultMonitor(ResultMonitor resultMonitor) {
 		this.resultMonitor = resultMonitor;
 	}
-	
+
 }
